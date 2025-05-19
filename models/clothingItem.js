049414462026d15bb10c3f1e6685e0150378c835
William <wedderburn95@yahoo.com>
2025-05-19 16:30:41 -0400

    Fixed a couple of  issues in clothingItem.js file

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const validator = require("validator");
 
-const user = require("./user.js");
+const user = require("./user");
 
 const clothingItemSchema = new mongoose.Schema({
   name: {
