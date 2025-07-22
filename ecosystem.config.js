c2797997ec25b0fa9f9638efd734bf95827d582f
William <wedderburn95@yahoo.com>
2025-07-22 10:00:31 -0400

    Updated ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,18 @@ module.exports = {
     {
       name: "se_project_express",
       script: "app.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_restarts: 5,
       env: {
         NODE_ENV: "production",
-        JWT_SECRET: "dev-secret", // ✅ add your actual secret here
+        JWT_SECRET: "dev-secret",
+        PORT: 3001,
+      },
+      env_production: {
+        NODE_ENV: "production",
+        JWT_SECRET: "dev-secret",
         PORT: 3001,
       },
     },
